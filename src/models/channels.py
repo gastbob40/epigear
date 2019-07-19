674b2b404a81012c8b87ef24a804d2a4792623fa
Baptiste <bat.bourdet@gmail.com>
2019-07-20 00:01:31 +0200

    feat(channels): channels added role.py modified with RoleDiscord channels.py added

---
+++ src/models/channels.py
@@ -0,0 +1,20 @@
+from typing import Dict
+from discord import Role as RoleDiscord
+from discord import PermissionOverwrite
+
+
+class Channel:
+    name: str
+    overwrites: Dict[RoleDiscord, PermissionOverwrite]
+
+    def __init__(self, name: str, overwrites: Dict[RoleDiscord, PermissionOverwrite]):
+        self.name = name
+        self.overwrites = overwrites
+
+
+class Category(Channel):
+    channels: Dict[str, Channel]
+
+    def __init__(self, name: str, overwrites: Dict[RoleDiscord, PermissionOverwrite], channels: Dict[str, Channel]):
+        super().__init__(name, overwrites)
+        self.channels = channels
