d2c4f2f06285c0259efcf6cd0812ef5d2787a2bd
gastbob40 <gastbob40@gmail.com>
2019-07-19 19:11:45 +0200

    perf(permissions_parser): close the file before$

--- src/yaml_parser/permissions_parser.py
+++ src/yaml_parser/permissions_parser.py
@@ -10,17 +10,17 @@ class PermissionGroupParser:
     def yaml_to_objects() -> List[PermissionGroup]:
         with open('config/perms_groups.yml', 'r') as stream:
             data = yaml.safe_load(stream)
-            perms_group = []
+        perms_group = []
 
-            # For all permission group
-            for perm_group_name in data:
-                perm_group = PermissionGroup(perm_group_name)
+        # For all permission group
+        for perm_group_name in data:
+            perm_group = PermissionGroup(perm_group_name)
 
-                # For all permission in a permission group
-                for perm_name in data[perm_group_name]:
-                    value = data[perm_group_name][perm_name]
-                    perm_group.permissions.append(Permission(perm_name, value))
+            # For all permission in a permission group
+            for perm_name in data[perm_group_name]:
+                value = data[perm_group_name][perm_name]
+                perm_group.permissions.append(Permission(perm_name, value))
 
-                perms_group.append(perm_group)
+            perms_group.append(perm_group)
 
-            return perms_group
+        return perms_group
