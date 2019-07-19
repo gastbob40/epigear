2878258d9761baede15852212d3b7898340e4649
Baptiste <bat.bourdet@gmail.com>
2019-07-19 22:31:44 +0200

    feat(role_parser): added role parser create role parser and modified role

--- src/index.py
+++ src/index.py
@@ -1,6 +1,12 @@
 from src.yaml_parser.permissions_parser import PermissionGroupParser
+from src.yaml_parser.role_parser import RoleParser
 
-a = PermissionGroupParser.yaml_to_objects()
-print(a)
+permissions_groups = PermissionGroupParser.yaml_to_objects()
+
+roles = RoleParser.yaml_to_objects(permissions_groups)
+
+for key in roles:
+    print('role : {} defined by {}'.format(key, str(roles[key])))
+
 
 
