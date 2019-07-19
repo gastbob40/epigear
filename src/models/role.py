579ef3a6239fbbcae3267192b22d1843267d3a8c
Baptiste <bat.bourdet@gmail.com>
2019-07-19 20:02:47 +0200

    feat(roles): added colour to Role

--- src/models/role.py
+++ src/models/role.py
@@ -1,18 +1,19 @@
 from src.models.permission import Permission
+from discord import colour
 
 
 class Role:
     name: str
-    color: str
-    permissions: Permission
+    color: colour
+    permission: Permission
     hoist: bool
     mentionable: bool
 
-    def __init__(self, name: str, color: str = None, permissions: Permissions = None,
+    def __init__(self, name: str, color_code: int = 0x000000, permission: Permission = None,
                  hoist: bool = False, mentionable: bool = False):
         self.name = name
-        self.color = color
-        self.permissions = permissions
+        self.color = colour.Color(color_code)
+        self.permissions = permission
         self.hoist = hoist
         self.mentionable = mentionable
 
