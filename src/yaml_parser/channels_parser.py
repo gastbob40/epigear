99dd2b0c03e05dc530cc40c055f64a7e8c1ce87b
Baptiste <bat.bourdet@gmail.com>
2019-07-20 00:52:18 +0200

    feat(channel_parser): channel parser to test

--- src/yaml_parser/channels_parser.py
+++ src/yaml_parser/channels_parser.py
@@ -2,13 +2,15 @@ from src.models.channels import Channel, Category
 from src.models.role import Role
 from src.models.permission_group import PermissionGroup
 from typing import Dict
+from discord import Role as RoleDiscord
+from discord import PermissionOverwrite
 import yaml
 
 
 class ChannelParser:
 
     @staticmethod
-    def yaml_to_objects(permissions_groups: Dict[str, PermissionGroup], roles: Dict[str, Role]) -> Dict[str, Role]:
+    def yaml_to_objects(permissions_groups: Dict[str, PermissionGroup], roles: Dict[str, Role]) -> Dict[str, Category]:
         with open('run/config_server/server_channels.yml', 'r') as stream:
             data = yaml.safe_load(stream)
 
@@ -18,11 +20,27 @@ class ChannelParser:
             channels = {}
 
             for channel_name in data[category_name]['channels']:
-                new_channel = Channel(data[category_name]['channels'][channel_name]['name'],
-                                      data[category_name]['channels'][channel_name]['overwrites'])
-                channels[channel_name]=new_channel
+                overwrites = ChannelParser.__overwrites_parser__(
+                    data[category_name]['channels'][channel_name]['overwrites'], permissions_groups, roles)
+                new_channel = Channel(data[category_name]['channels'][channel_name]['name'], overwrites)
+                channels[channel_name] = new_channel
 
-            new_category = Category(data[category_name]['name'], data[category_name]['overwrites'], channels)
+            overwrites = ChannelParser.__overwrites_parser__(
+                data[category_name]['overwrites'], permissions_groups, roles)
+            new_category = Category(data[category_name]['name'], overwrites, channels)
             categories[category_name] = new_category
 
-        return roles
+        return categories
+
+    @staticmethod
+    def __overwrites_parser__(raw_overwrites: Dict[str, str],permissions_groups: Dict[str, PermissionGroup],
+                            roles: Dict[str, Role]) -> Dict[RoleDiscord, PermissionOverwrite]:
+
+        overwrites = {}
+
+        for role_name in raw_overwrites:
+            role_discord = roles[role_name].role_discord
+            perm = permissions_groups[raw_overwrites[role_name]].permissions_overwrite
+            overwrites[role_discord] = perm
+
+        return overwrites
