032b755af45e8c455c772f5bae9cb3b1cc889b0e
gastbob40 <gastbob40@gmail.com>
2019-07-19 22:52:02 +0200

    feat(role_promo_parser): finish role promo parser

--- src/index.py
+++ src/index.py
@@ -1,12 +1,11 @@
 from src.yaml_parser.permissions_parser import PermissionGroupParser
 from src.yaml_parser.role_parser import RoleParser
+from src.yaml_parser.role_promo_parser import RolePromoParser
 
 permissions_groups = PermissionGroupParser.yaml_to_objects()
-
 roles = RoleParser.yaml_to_objects(permissions_groups)
+promo_roles = RolePromoParser.yaml_to_objects(2024, permissions_groups)
 
-for key in roles:
-    print('role : {} defined by {}'.format(key, str(roles[key])))
 
 
 
--- src/yaml_parser/role_promo_parser.py
+++ src/yaml_parser/role_promo_parser.py
@@ -12,15 +12,27 @@ class RolePromoParser:
             data = yaml.safe_load(stream)
 
         colors = RolePromoParser.__get_colors__(current_promo, data)
-
-
-
         roles = {}
+        current_promo += 1
+
+        for role_name in data['roles_template']:
+            new_role = Role(str(current_promo), colors[str(current_promo)],
+                            permissions_groups[data['roles_template'][role_name]['permissions']],
+                            data['roles_template'][role_name]['hoist'],
+                            data['roles_template'][role_name]['mentionable'])
+
+            roles[role_name] = new_role
+            current_promo -= 1
+
+        return roles
 
     @staticmethod
     def __get_colors__(current_promo: int, data: any) -> Dict[str, int]:
         colors = {}
-        for i in range(current_promo - 4, current_promo + 1):
-            colors[f'{i}'] = data['promos'][i]['color']
+        for i in range(current_promo - 4, current_promo + 2):
+            try:
+                colors[f'{i}'] = data['promos'][i]['color']
+            except:
+                colors[f'{i}'] = 0xffffff
         return colors
 
