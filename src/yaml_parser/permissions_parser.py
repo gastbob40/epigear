8f9cc349695a4a0f4d147e2d1b05df828abc98e1
bat021 <43823976+bat021@users.noreply.github.com>
2019-07-23 13:14:54 +0200

    fix(permissions_parser): fix to allow null permissions grou^p

--- src/yaml_parser/permissions_parser.py
+++ src/yaml_parser/permissions_parser.py
@@ -19,8 +19,9 @@ class PermissionGroupParser:
         for perm_group_name in data:
             logger.debug('Parsing perm group {}'.format(perm_group_name))
             perm_group = PermissionGroup(perm_group_name)
-            perm_group.permissions.update(**data[perm_group_name])
-            perm_group.permissions_overwrite.update(**data[perm_group_name])
+            if(len(data[perm_group_name]) != 0):
+                perm_group.permissions.update(**data[perm_group_name])
+                perm_group.permissions_overwrite.update(**data[perm_group_name])
             perm_groups[perm_group_name] = perm_group
 
         logger.info('{} permission groups parsed'.format(len(data)))
